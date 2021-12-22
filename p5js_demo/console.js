1f2573114e3339f65a8fd8a8f4346df027842373
Brett <cauc_yingbo_li@163.com>
2021-12-22 17:10:50 +0800

    Add files via upload

--- p5js_demo/console.js
+++ p5js_demo/console.js
@@ -1,3 +1,22 @@
-document.body.addEventListener('touchmove', function (e) {
-  e.preventDefault(); //阻止默认的处理方式(阻止下拉滑动的效果)
-}, {passive: false}); //passive 参数不能省略，用来兼容ios和android
+var overscroll = function (el) {
+    el.addEventListener('touchstart', function () {
+        var top = el.scrollTop
+            , totalScroll = el.scrollHeight
+            , currentScroll = top + el.offsetHeight;
+        if (top === 0) {
+            el.scrollTop = 1;
+        } else if (currentScroll === totalScroll) {
+            el.scrollTop = top - 1;
+        }
+    });
+    el.addEventListener('touchmove', function (evt) {
+        if (el.offsetHeight < el.scrollHeight)
+            evt._isScroller = true;
+    });
+}
+// overscroll(document.querySelector('.scroll'));
+document.body.addEventListener('touchmove', function (evt) {
+    if (!evt._isScroller) {
+        evt.preventDefault();
+    }
+});
